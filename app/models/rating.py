58871d217a617266cd30de9264e04af012f7de2b
Bill <bill.omondi@student.moringaschool.com>
2025-07-16 22:46:06 +0300

    group_member model

--- app/models/rating.py
+++ app/models/rating.py
@@ -5,6 +5,6 @@ class Rating(db.Model):
 
     id = db.Column(db.Integer, primary_key = True)
     value = db.Column(db.Integer, nullable = False)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    recipe_id = id.Column(db.Integer, db.ForeignKey('recipes.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable = False)
+    recipe_id = id.Column(db.Integer, db.ForeignKey('recipes.id'), nullable = False)
     created_at = id.Column(db.Datetime, default = db.func.current_timestamp())
