9bc642379bc0bd13bc731de99fcdd29fba9a9e84
Richard <olella93@gmail.com>
2025-07-18 00:37:50 +0300

    feat: enhance authentication with validation and JWT tokens
    - Add required field validation for registration
    - Generate JWT access tokens on login
    - Return user data with authentication response

--- app/routes/auth_routes.py
+++ app/routes/auth_routes.py
@@ -1,12 +1,21 @@
 from flask import Blueprint, jsonify, request  
 from app.models.user import User
 from app.db import db
+from flask_jwt_extended import create_access_token
+from app.schemas.user_schema import UserSchema
+user_schema = UserSchema()
+
+
 
 auth_bp = Blueprint('auth', __name__)
 
 @auth_bp.route('/register', methods = ['POST'])
 def register():
     data = request.get_json()
+    required = ['username', 'email', 'password']
+    if not all(field in data for field in required):
+        return jsonify({"error": "Missing required fields"}), 400
+
     if User.query.filter_by(username = data['username']).first():
         return jsonify({"error": "Username already exists"}), 400
     
@@ -22,18 +31,24 @@ def register():
     db.session.add(user)
     db.session.commit()
 
-    return jsonify({"message": "User created", "user_id": user.id}), 201
+    return user_schema.jsonify(user), 201
 
 @auth_bp.route('/login' , methods =['POST'] )
 def login():
     data = request.get_json()
+    required = ['username', 'password']
+    if not all(field in data for field in required):
+     return jsonify({"error": "Missing username or password"}), 400
+
     user = User.query.filter_by(username = data['username']).first()
 
     if not user or not user.check_password(data['password']):
         return jsonify({"error": "Incorect username or password"}), 401
     
+    access_token = create_access_token(identity=user.id)
     return jsonify({
         "message": "Login successful",
+        "token": access_token,
         "user_id": user.id,
         "username": user.username
     })
