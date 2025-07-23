fd960a335b9453976889bd9022d50c688deaf48c
Andrew Bariu <andrew.bariu@student.moringaschool.com>
2025-07-23 19:20:14 +0300

    added comment functionality with full POST endpoint,JWT auth, and schema nesting

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,8 +1,10 @@
 from .auth_routes import auth_bp
 from .recipe_routes import recipe_bp
 from .group_routes import group_bp
+from .comment_routes import comment_bp
 
 def init_routes(app):
-    app.register_blueprint(auth_bp, url_prefix = '/api/auth')
-    app.register_blueprint(recipe_bp, url_prefix = '/api')
-    app.register_blueprint(group_bp, url_prefix = '/api')
+    app.register_blueprint(auth_bp, url_prefix='/api/auth')
+    app.register_blueprint(recipe_bp, url_prefix='/api')
+    app.register_blueprint(group_bp, url_prefix='/api')
+    app.register_blueprint(comment_bp)
