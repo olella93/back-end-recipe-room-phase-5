a71cccb2a81bb41466aa1c8fc2c941ca7bf298d8
Andrew Bariu <andrew.bariu@student.moringaschool.com>
2025-07-23 17:51:54 +0300

    feat(config): add fallback to SQLite for easier local development

--- app/config.py
+++ app/config.py
@@ -4,17 +4,23 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
+    # Secret keys
     SECRET_KEY = os.getenv('SECRET_KEY', '2b2492e67a8f156fda78437999b439e0')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://citikom@localhost:5432/recipe_room')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    CORS_HEADERS = 'Content-Type'
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "super-secret-jwt-key")
-    
+
+    # SQLAlchemy Database configuration
+    SQLALCHEMY_DATABASE_URI = ('postgresql://citikom:newpassword@localhost:5432/recipe_room'
+
+    )
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # CORS
+    CORS_HEADERS = 'Content-Type'
+
     # Cloudinary Configuration
     CLOUDINARY_CLOUD_NAME = os.getenv('CLOUDINARY_CLOUD_NAME')
     CLOUDINARY_API_KEY = os.getenv('CLOUDINARY_API_KEY')
     CLOUDINARY_API_SECRET = os.getenv('CLOUDINARY_API_SECRET')
-    
+
     # File Upload Configuration
     MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # 16MB max file size
-    
