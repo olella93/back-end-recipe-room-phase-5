37de3b3630d99f32ce9d99b6952a9a58146cbe05
Andrew Bariu <andrew.bariu@student.moringaschool.com>
2025-07-24 17:32:44 +0300

    resolved blueprint name conflict by renaming auth_bp

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -9,5 +9,4 @@ def init_routes(app):
     app.register_blueprint(recipe_bp, url_prefix='/api')
     app.register_blueprint(group_bp, url_prefix='/api')
     app.register_blueprint(comment_bp)
-    app.register_blueprint(auth_bp, url_prefix='/api/auth')
     app.register_blueprint(bookmark_bp, url_prefix='/api')
