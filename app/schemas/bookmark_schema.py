62b5e990278a8752791336c852fb751a95efdab9
Richard <olella93@gmail.com>
2025-07-31 17:20:14 +0300

    chore: update bookmark_schema.py as is

--- app/schemas/bookmark_schema.py
+++ app/schemas/bookmark_schema.py
@@ -1,5 +1,7 @@
 from app.extensions import ma
 from app.models.bookmark import Bookmark
+from app.schemas.recipe_schema import RecipeSchema
+from marshmallow import fields
 
 class BookmarkSchema(ma.SQLAlchemySchema):
     class Meta:
@@ -9,3 +11,4 @@ class BookmarkSchema(ma.SQLAlchemySchema):
     id = ma.auto_field()
     user_id = ma.auto_field()
     recipe_id = ma.auto_field()
+    recipe = fields.Nested(RecipeSchema)
